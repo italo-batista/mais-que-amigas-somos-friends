76af0551dadce4a233fb4a3de5875531008c754a
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2017-10-10 02:34:28 -0300

    Find clusters

--- plot_network.R
+++ plot_network.R
@@ -1,7 +1,6 @@
 # Load package
 library(networkD3)
 
-
 # Load data
 data(MisLinks)
 data(MisNodes)
@@ -14,15 +13,23 @@ my_nodes = data.frame(
 )
 
 my_links.source = c('a', 'a', 'e', 'e')
-my_links.target = c('b', 'c', 'd', '' )
+my_links.target = c('b', 'c', 'd', 'f' )
 my_links = data.frame(
   source = my_links.source,
   target = my_links.target,
   value = c(1, 1, 1, 1)
 )
 
-
 forceNetwork(Links = my_links, Nodes = my_nodes,
              Source = "source", Target = "target",
              Value = "value", NodeID = "name",
              Group = "group", opacity = 0.8)
+
+
+
+#-------------------
+
+source = c('a', 'a', 'e', 'e')
+target = c('b', 'c', 'd', 'f')
+networkData <- data.frame(source, target)
+simpleNetwork(networkData)
